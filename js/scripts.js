6f27cd32fcd36130f83ff89f616dc146af52ed74
Horváth Gergely <gergely0703@gmail.com>
2022-04-12 19:46:21 +0200

    finish all tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var szamol = () => {
-    
+let szamol = () => {
+    let gyermekek = document.getElementById("gyermekek").value;
+    let felnottek = document.getElementById("felnottek").value;
+    let osszeg;
+
+    if (gyermekek < 0 || felnottek < 0 || gyermekek % 1 != 0 || felnottek % 1 != 0) {
+        alert("A megadott érték nem egész szám! A létszám legyen legalább 0");
+    } else {
+        osszeg = gyermekek * 28000 + felnottek * 40000;
+        document.getElementById("result").innerHTML = osszeg;
+        document.getElementById("alert").classList.add('show');
+    }
 }
